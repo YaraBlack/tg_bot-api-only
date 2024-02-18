04445ecf9f867d02bc0da318fe8d4ae3833465ce
YaraBlack <yara26072000@gmail.com>
2024-02-18 23:32:31 +0200

    Make the bot work properly
    1. Import a few packages.
    2. Add ConversationHandler
    3. Use formatter (Black)
    4. Disable standard MessageHandler
    5. Add 4 new functions to support the ConversationHandler

--- tg_bot.py
+++ tg_bot.py
@@ -1,14 +1,19 @@
 import logging
-from typing import Final
 from config import *
-from telegram import Update
-from telegram.ext import Application, MessageHandler, CommandHandler, filters,\
-ContextTypes
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    CommandHandler,
+    filters,
+    ContextTypes,
+    ConversationHandler,
+)
 
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
 )
 
 # Set higher logging level for httpx to avoid all GET and POST
@@ -17,79 +22,198 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+# Set constants
+admins_ids: tuple = ADMINS_IDS
+ANON_STEP, PROPOSED_CONTENT_STEP = 0, 1
+
+post_proposal_user = [None, None]
+
+
 # Command handlers
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user.username
-    await update.message.reply_text(f"Привіт, {user}! Для списку команд \
-скористайся командою /help")
-    
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('''/help - список команд
+    await update.message.reply_text(
+        f"Привіт, {user}! Для списку команд \
+скористайся командою /help"
+    )
+
+
+async def helpCommand(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text(
+        """/help - список команд
 /post - надіслати пост
-/fumo - Фумізація повідомлень ᗜˬᗜ''')
-    
-async def fumo_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+/fumo - Фумізація повідомлень ᗜˬᗜ"""
+    )
+
+
+async def getUserIdCommand(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text(
+        f"Твій Telegram User ID: {update.effective_user.id}"
+    )
+
+
+async def fumoCommand(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
     user_message = update.message.text
     await update.message.reply_text(f"{user_message.replace('/fumo', '')} ᗜˬᗜ")
- 
-# Add functionality, update response, send content to me    
-async def post_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message
-    print(f'message: ', user_message)
-    await update.message.reply_text('''Дякую за пропозицію.
-Пост надіслано адміністрації.''')
+
+
+# Add functionality, update response, send content to me
+async def postCommand(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+
+    reply_keyboard = [["Так", "Ні"]]
+
+    await update.message.reply_text(
+        "Чи бажаєш відправити пост анонімно?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            one_time_keyboard=True,
+        ),
+    )
+
+    return ANON_STEP
+
+
+async def checkAnon(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+
+    if (
+        update.message.text.lower() == "так"
+        or update.message.text.lower() == "ні"
+    ):
+        if update.message.text.lower() == "так":
+            pass
+        elif update.message.text.lower() == "ні":
+            global post_proposal_user
+            post_proposal_user[0] = update.message.from_user.id
+            post_proposal_user[1] = update.message.from_user.username
+        await update.message.reply_text("Надішли свій пост:")
+        return PROPOSED_CONTENT_STEP
+    else:
+        await update.message.reply_text(
+            "Невірний варіант відповіді. Спробуй ще раз."
+        )
+        postCommand()
+
+
+async def proposeContent(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    global post_proposal_user
+    await update.message.reply_text(
+        "Дякую! Твій пост надісланий на перевірку адміністрації!"
+    )
+    user_id = post_proposal_user[0]
+    username = post_proposal_user[1]
+    for admin_id in ADMINS_IDS:
+        await context.bot.send_message(
+            chat_id=admin_id,
+            text=f"@{username}",
+        )
+        await context.bot.forward_message(
+            chat_id=admin_id,
+            from_chat_id=update.effective_chat.id,
+            message_id=update.message.message_id,
+        )
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text("Відмінено.")
+    return ConversationHandler.END
+
 
 # Responses
 def handle_response(text: str) -> str:
-    
-    processed:str = text.lower()
-    
-    if 'fumo' in processed:
-        return 'Fumo enjoyer!'
-    
-    return '''Я не вмію відповідати на повідомлення.
-Використай команду зі списку /help.'''
+
+    processed: str = text.lower()
+
+    if "fumo" in processed:
+        return "Fumo enjoyer!"
+
+    return """Я не вмію відповідати на повідомлення.
+Використай команду зі списку /help."""
+
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type: str = update.message.chat.type # group or private
+    message_type: str = update.message.chat.type  # group or private
     text: str = update.message.text
-    
-    print(f'User {update.message.chat.username} ({update.message.chat.id}) \
-in {message_type}: "{text}"')
-    
+
+    print(
+        f'User {update.message.chat.username} ({update.message.chat.id}) \
+in {message_type}: "{text}"'
+    )
+
     if message_type == "group":
         if BOT_NAME in text:
-            new_text: str = text.replace(BOT_NAME, '').strip()
+            new_text: str = text.replace(BOT_NAME, "").strip()
             response: str = handle_response(new_text)
         else:
             return
     else:
         response: str = handle_response(text)
-        
-    print('Bot:', response)
+
+    print("Bot:", response)
     await update.message.reply_text(response)
 
+
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
-    
-    
+    print(f"Update {update} caused error {context.error}")
+
 
 if __name__ == "__main__":
-    print('Starting bot...')
+    print("Starting bot...")
     app = Application.builder().token(BOT_TOKEN).build()
-    
+
     # Commands
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
-    app.add_handler(CommandHandler("post", post_command))
-    app.add_handler(CommandHandler("fumo", fumo_command))
-    
+    app.add_handler(CommandHandler("help", helpCommand))
+    app.add_handler(CommandHandler("id", getUserIdCommand))
+    app.add_handler(CommandHandler("fumo", fumoCommand))
+
     # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
-    
+    # app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Conversation
+    app.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("post", postCommand)],
+            states={
+                ANON_STEP: [
+                    MessageHandler(
+                        filters.Regex("^(Так|Ні|так|ні)$"), checkAnon
+                    )
+                ],
+                PROPOSED_CONTENT_STEP: [
+                    MessageHandler(
+                        filters.PHOTO
+                        | filters.ANIMATION
+                        | filters.AUDIO
+                        | filters.VIDEO
+                        | filters.VOICE
+                        | filters.TEXT
+                        | filters.Sticker.ALL,
+                        proposeContent,
+                    )
+                ],
+            },
+            fallbacks=[CommandHandler("cancel", cancel)],
+        )
+    )
+
     # Errors
     app.add_error_handler(error)
-    
+
     # Bot polling
-    print('Polling...')
-    app.run_polling(poll_interval=3)
+    print("Polling...")
+    app.run_polling(poll_interval=3, allowed_updates=Update.ALL_TYPES)
+
+# Bugs section - temporary
+# - 'else' part in checkAnon is not working
+# add additional commands in Botfather
